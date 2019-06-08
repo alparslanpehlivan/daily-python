567c5a05cdeb26303a6a3b811a3d5f86f8bd37e1
Pehlivan <pehliva@emea.corpdir.net>
2019-06-08 15:32:30 +0300

    -

--- try_20190608/fibonacci.py
+++ try_20190608/fibonacci.py
@@ -7,10 +7,18 @@ def fib(n):
 def Main():
     parser=argparse.ArgumentParser()
     parser.add_argument("num", help="The fibonacci number wish to calculate", type=int)
+    parser.add_argument("-o", "--output", help="Output results to a file",\
+                        action="store_true")
     args = parser.parse_args()
 
     result = fib(args.num)
     print("The "+str(args.num)+"th fib number is "+str(result))
 
+    if args.output:
+        f= open("fibonacci.txt", "a")
+        f. write(str(result)+'\n')
+
 if __name__ == '__main__':
     Main()
+
+#fibonacci.py 25 -o
