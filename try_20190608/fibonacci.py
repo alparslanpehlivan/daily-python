c8219268fcebadecc5c1338f90361c0bc24625f8
alparslanpehlivan <50515844+alparslanpehlivan@users.noreply.github.com>
2019-06-08 16:55:36 +0300

    -

--- try_20190608/fibonacci.py
+++ try_20190608/fibonacci.py
@@ -6,14 +6,22 @@ def fib(n):
     return a
 def Main():
     parser=argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="store_true")
+    group.add_argument("-q","--quiet", action="store_true")
     parser.add_argument("num", help="The fibonacci number wish to calculate", type=int)
     parser.add_argument("-o", "--output", help="Output results to a file",\
                         action="store_true")
     args = parser.parse_args()
 
     result = fib(args.num)
-    print("The "+str(args.num)+"th fib number is "+str(result))
 
+    if args.verbose:
+        print("The "+str(args.num)+"th fib number is "+str(result))
+    elif args.quiet:
+        print(result)
+    else:
+        print("Fib("+str(args.num)+") = "+str(result))
     if args.output:
         f= open("fibonacci.txt", "a")
         f. write(str(result)+'\n')
