e226ec520a4f2d8683b5fbcdc8cb94be44f487aa
Alparslan <50515844+alparslanpehlivan@users.noreply.github.com>
2019-06-15 00:02:21 +0300

    -

---
+++ try_20190615-pyautogui/try_20190615/pyautogui_mouse.py
@@ -0,0 +1,20 @@
+import pyautogui
+pyautogui.size()
+width, height = pyautogui.size()
+print(pyautogui.size())
+print(pyautogui.position())
+#pyautogui.moveTo(10,10) #to move specific coordinate
+#pyautogui.moveTo(10,10,duration=1.5)
+#pyautogui.moveRel(200, -10, duration=3) #to move relative location
+#print(pyautogui.position())
+
+#pyautogui.moveTo(1208,277)
+#pyautogui.click()
+
+#pyautogui.click(1208,277)
+
+#pyautogui.doubleClick(1208,277)
+#pyautogui.rightClick(1208,277)
+#pyautogui.middleClick(1208,277)
+
+#pyautogui.displayMousePosition() #live coordinateexit(
