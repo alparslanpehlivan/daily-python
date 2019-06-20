a7cc0390920e97e5635ce90277232fe0fc4d2371
Alparslan <50515844+alparslanpehlivan@users.noreply.github.com>
2019-06-20 21:49:48 +0300

    -

---
+++ try_20190620-pyautogui-keyboard/pyautogui_keyboard.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import pyautogui
+
+print(pyautogui.position())
+
+pyautogui.click(218,505)
+
+pyautogui.typewrite("Hello World")
+
+pyautogui.typewrite(['a','b','left'])
+
+#pyautogui.KEYBOARD_KEYS
+    
+pyautogui.press("f1")
+
+pyautogui.hotkey("ctrl","o")
+
+
+
+
+
+
+
+
