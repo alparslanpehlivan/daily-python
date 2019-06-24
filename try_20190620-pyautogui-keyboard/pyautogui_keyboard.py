b9ee2a498ce426c0f0dab03f8c2c071ce36e1eac
Alparslan <50515844+alparslanpehlivan@users.noreply.github.com>
2019-06-24 21:28:34 +0300

    -

--- try_20190620-pyautogui-keyboard/pyautogui_keyboard.py
+++ try_20190620-pyautogui-keyboard/pyautogui_keyboard.py
@@ -15,6 +15,15 @@ pyautogui.press("f1")
 
 pyautogui.hotkey("ctrl","o")
 
+#pyautogui.click()
+#pyautogui.typewrite("Hello World")
+#pyautogui.screenshot("C:\\example.jpg") take screenshot
+#pyautogui.locateOnScreen("C:\\example.jpg") #(900(x),316(y),56(width),56(height))
+#pyautogui.locateCenterOnScreen("C:\\example.jpg") #center of picture
+#pyautogui.moveTo((x,y),duration=1) #to move mouse to a specific coordinate
+#pyautogui.click((x,y))
+#pyautogui.click(x,y)
+
 
 
 
