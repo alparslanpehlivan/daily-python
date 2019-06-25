4378939e9d02c8e9e4316fbb78b6f48002401bd7
Alparslan <50515844+alparslanpehlivan@users.noreply.github.com>
2019-06-25 18:34:21 +0300

    -

--- try_20190625-pandigital/pandigital.py
+++ try_20190625-pandigital/pandigital.py
@@ -9,6 +9,6 @@ print(mylist)
 
 if "1" in mylist and "2" in mylist and "3" in mylist and "4" in mylist and \
     "5" in mylist and "6" in mylist and "9" in mylist and "8"  in mylist and "7" in mylist:
-    print("ok")
+    print("yes")
 else:
-    print("dok")
+    print("no")
